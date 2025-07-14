2c3f205c15574c86596f32a91a944c8f4f82953e
Ananthu <ananthusmk@gmail.com>
2025-07-14 17:24:54 +0530

    final commit

--- frontend/src/components/CMS/Cms.jsx
+++ frontend/src/components/CMS/Cms.jsx
@@ -3,47 +3,61 @@ import './Cms.css'
 import { AppContext } from '../../context/AppContext'
 import axios from 'axios'
 
-const Cms = ({setCms}) => {
-  const [showEdit, setShowEdit] = useState(false)
-  const [updateHeadline, setUpdateHeadline] = useState('') 
-  const {url, headline, setHeadline} = useContext(AppContext)
-  
-  
+const Cms = ({ setCms }) => {
+    const [showEdit, setShowEdit] = useState(false)
+    const [errMsg, setErrMsg] = useState('')
+    const [updateHeadline, setUpdateHeadline] = useState('')
+    const { url, headline, setHeadline } = useContext(AppContext)
+
+
     const addHeadline = async () => {
         try {
             const response = await axios.post(url + 'heading', {
                 heading: updateHeadline
             })
-            if(response.status === 200){
+            if (response.status === 200) {
                 setHeadline(response.data.headline.text)
                 setShowEdit(false)
                 setCms(false)
+                setErrMsg('')
+            }
+            else {
+
+                setErrMsg(response.data.error)
             }
         } catch (error) {
             console.log('Failed to add headline', error);
-            
+
         }
     }
-    
 
-  return (
-    <div className='cms-container'>
-    <h1 onClick={() => setCms(false)} style={{color:'#FFF', top: '20px', right: '20px', fontSize: '24px', fontWeight: '700',
-        cursor:'pointer', position:'absolute'}}>X</h1>
-    <div className='edit-container'>
-        <button className='buttons h-btn'>Heading</button>
-        <div className='text-section'>
-            {showEdit? <textarea rows={6} value={updateHeadline} onChange={(e) => setUpdateHeadline(e.target.value) }  className='text-container'/> : <div className='text-container'>
-                {headline}
-            </div>}
-            <div className='btn-container'>
-                <button className='buttons btn' onClick={() => setShowEdit(true)}>Edit</button>
-                <button onClick={addHeadline} className='buttons btn'>Save</button>
+    console.log('hdjd', errMsg)
+
+    return (
+        <div className='cms-container'>
+            <h1 onClick={() => setCms(false)} style={{
+                color: '#FFF', top: '20px', right: '20px', fontSize: '24px', fontWeight: '700',
+                cursor: 'pointer', position: 'absolute'
+            }}>X</h1>
+            <div className='edit-container'>
+                <button className='buttons h-btn'>Heading</button>
+                <div className='text-section'>
+                    {showEdit ? <textarea rows={6} value={updateHeadline} onChange={(e) => setUpdateHeadline(e.target.value)} className='text-container' /> : <div className='text-container'>
+                        {headline}
+                    </div>}
+                    {errMsg.length !== 0 && (
+                        <p style={{ color: 'red', fontSize: '15px', position: 'absolute' }}>
+                            {errMsg}
+                        </p>
+                    )}
+                    <div className='btn-container'>
+                        <button className='buttons btn' onClick={() => setShowEdit(true)}>Edit</button>
+                        <button onClick={addHeadline} disabled={updateHeadline.length === 0} className='buttons btn'>Save</button>
+                    </div>
+                </div>
             </div>
         </div>
-    </div>
-    </div>
-  )
+    )
 }
 
 export default Cms
